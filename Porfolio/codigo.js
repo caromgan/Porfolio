5195703ae9c7b8dc16e598fe8d165ea2bcf2d904
CAROLINA MABEL GANDIN <126636103+caromgan@users.noreply.github.com>
2025-09-20 23:36:45 -0300

    Porfolio listo!
    Se sumo el menú y ajustes de estilo

---
+++ Porfolio/codigo.js
@@ -0,0 +1,10 @@
+
+function AgregarComentario ()
+{
+var nom = document.getElementById("textNombre").value;
+alert ("Gracias " + nom +" por enviarme tus comentarios ");
+var Apellido1 = document.getElementById("textApellido").value;
+var Mail1 = document.getElementById("textMail").value;
+var Comentario1 = document.getElementById("textComentario").value;
+console.log ("HOla "+ nom +""+ Apellido1+" Tu correo es: " + Mail1 +" Y tu comentario: "+ Comentario1);
+}
